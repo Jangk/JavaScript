020c008aa7b704ec9bca05e8fd4cf78b1e98589d
장재완 <wkdwodhks7@naver.com>
2023-02-03 17:35:55 +0900

    오늘꺼 커밋되는지 확인

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-function func()
+const DEFAULT_FONTSIZE = "1.3em";
+
+
+document.getElementById("HeadText").onclick = 
+function()
 {
-    var strName     = prompt("이름 입력");
-    var fAge        = prompt("나이");
-    document.querySelector("#Test").innerHTML = strName + fAge;
-}
-
-function funcOpen()
-{
-    var a = document.querySelector("#openItem");
-    alert(a);
-    
-}
-
-function funcClose()
-{
-}
+    if(this.style.fontSize === DEFAULT_FONTSIZE)
+    {
+        this.style.fontSize = "5em";
+        this.style.textAlign = "left";
+    }
+    else
+    {
+        this.style.fontSize = DEFAULT_FONTSIZE
+        this.style.textAlign = "center";
+    }
+};
 
 
-let gameobject = {
-    name: "gg",
-    other : "몰루"
-}
-var strName         = prompt("이름 입력");
-var dateNowDate     = new Date();               
-var strDateDisplay  = dateNowDate.toLocaleTimeString();
-Math.floor(Math.random());
-document.write("<big>" + strName + "님이 "+ strDateDisplay + "에 입장하셨습니다.");
-
-``
-//alert("메세지 박스");
