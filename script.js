e796e859c7f5f623cfdf52a54be3f946139784de
장재완 <wkdwodhks7@naver.com>
2023-02-05 17:36:31 +0900

    async 비동기 처리2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const promise1 = new Promise((resolve, reject) => {
     // 비동기 성공시 resolve 호출
     // 비동기 실패시 reject 호출
-    resolve();
+    // resolve와 reject는 한개만 실행되기 때문에 밑의 reject는 무시됨.
+    resolve();        
+    reject();
   });
   promise1
     .then(() => {
@@ -18,23 +20,24 @@ const promise1 = new Promise((resolve, reject) => {
 
 
 // async를 이용한 비동기 처리
-//async function myCallBack()
-//{
-//    return setTimeout(function (){}, 5000);
-//}
-//myCallBack().then
-//(
-//    (result)=>
-//    {
-//        alert("result");
-//    }
-//)
-//.catch
-//(
-//
-//)
+const iFlag = true;
+// async 함수는 promise를 리턴함
+// promise객체가 아닌값을 리턴할땐 promise 객체로 변환해서 반환함.
+async function asyncFunc()
+{
+  if(iFlag)
+    return "성공";
+  else
+    return console.error("실패");
+}
 
-
-
-
-    
+const promise2 = asyncFunc();
+promise2.then((value)=>{
+  console.log(value)
+})
+.catch((error)=>{
+  console.log(error)
+})
+.finally(
+  console.log("Finally")
+)
